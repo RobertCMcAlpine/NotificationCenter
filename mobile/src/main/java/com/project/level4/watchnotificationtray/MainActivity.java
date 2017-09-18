11e0c2cc3342ec90c4f05f3d368698188a256cfa
Robert McAlpine <robert.mcalpine26@gmail.com>
2017-09-18 16:34:33 +0100

    (1) fixed device rotation problem. (2) cleaned up code.

--- mobile/src/main/java/com/project/level4/watchnotificationtray/MainActivity.java
+++ mobile/src/main/java/com/project/level4/watchnotificationtray/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
@@ -33,8 +34,11 @@ import java.util.List;
  * the wearable companion app
  */
 
+
+//TODO sort orientation change problems.
+
 public class MainActivity extends PreferenceActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, SharedPreferences.OnSharedPreferenceChangeListener {
+        GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleApiClient googleClient;
     private static List<ApplicationData> appInfoList;
@@ -43,11 +47,6 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment(), "PreferenceFragment").commit();
-        PreferenceManager.setDefaultValues(this, getResources().getString(R.string.shared_pref_key),
-                Context.MODE_PRIVATE, R.xml.preferences, false);
-//        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
-
         googleClient = new GoogleApiClient.Builder(this)
                 .addApi(Wearable.API)
                 .addConnectionCallbacks(this)
@@ -55,9 +54,16 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
                 .build();
 
         initialiseNotificationManagement();
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+            PreferenceManager.setDefaultValues(this, getResources().getString(R.string.shared_pref_key),
+                    Context.MODE_PRIVATE, R.xml.preferences, false);
+        }
+
     }
 
-    private void initialiseNotificationManagement(){
+    private void initialiseNotificationManagement() {
         PackageManager pm = getPackageManager();
         List<ApplicationInfo> apps = pm.getInstalledApplications(0);
         List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
@@ -65,9 +71,9 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
         List<ApplicationData> mInstalledAppInfo;
         mInstalledAppInfo = new ArrayList<ApplicationData>();
 
-        for(ApplicationInfo app : apps) {
+        for (ApplicationInfo app : apps) {
             //checks for flags; if flagged, check if updated system app
-            if((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+            if ((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
                 installedApps.add(app);
                 //it's a system app, not interested
             } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
@@ -78,55 +84,13 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
             }
         }
 
-        for (int i=0; i<installedApps.size(); i++){
+        for (int i = 0; i < installedApps.size(); i++) {
             String title = pm.getApplicationLabel(installedApps.get(i)).toString();
             String pkg = installedApps.get(i).packageName;
             Drawable icon = pm.getApplicationIcon(installedApps.get(i));
             mInstalledAppInfo.add(new ApplicationData(title, pkg, icon));
         }
         appInfoList = mInstalledAppInfo;
-    }
-
-
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals(getResources().getString(R.string.notification_limit_key))) {
-            DataMap dataMap = new DataMap();
-            String limit = sharedPreferences.getString(key, "10");
-
-            // validate limit, if limit is out of bounds, write valid limit back to sharedPreferences
-            limit = validateLimit(limit);
-
-            dataMap.putString("package", "com.project.level4.watchnotificationtray");
-            dataMap.putString("title", "Settings");
-            dataMap.putString("text", "Notification History limit set to " + limit);
-            dataMap.putString("limit", limit);
-
-//            Log.i("MainActivity", "created DataMap for setting changes in sharedPreferences");
-
-            String WEARABLE_DATA_PATH = "/wearable_data";
-
-            //Requires a new thread to avoid blocking the UI
-            new SendToDataLayerThread(WEARABLE_DATA_PATH, dataMap, googleClient).start();
-//            Log.i("MainActivity", "starting SendToDataLayerThread");
-        }
-    }
-
-    public String validateLimit(String newValue){
-        try {
-            int limit = Integer.parseInt(newValue);
-            if (limit <= 100 && limit >= 10){
-                return newValue;
-            } else if (limit > 100){
-                newValue = "100";
-                return newValue;
-            } else if (limit < 10){
-                newValue = "10";
-                return newValue;
-            }
-        } catch (Exception e){
-            newValue = "10";
-        }
-        return newValue;
     }
 
 
@@ -137,9 +101,10 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
             PreferenceManager.setDefaultValues(getActivity(),
                     R.xml.preferences, false);
             addPreferencesFromResource(R.xml.preferences);
+            setRetainInstance(true);
 
             Preference delete = findPreference(getResources().getString(R.string.delete_key));
-            delete.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener(){
+            delete.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
                     new AlertDialog.Builder(getActivity())
@@ -154,7 +119,7 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
                                     dataMap.putString("delete", Boolean.toString(true));
 //                                    Log.i("MainActivity", "created DataMap for setting changes in sharedPreferences");
 
-                                    ((MainActivity)getActivity()).deleteNotifications(dataMap);
+                                    ((MainActivity) getActivity()).deleteNotifications(dataMap);
                                 }
                             })
                             .setNegativeButton(R.string.neg, new DialogInterface.OnClickListener() {
@@ -169,10 +134,10 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
             });
 
             Preference manage = findPreference(getResources().getString(R.string.notification_manager_key));
-            manage.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener(){
+            manage.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    getFragmentManager().beginTransaction().replace(android.R.id.content, new MyNotificationManagementFragment(), "PreferenceFragment").addToBackStack("Settings").commit();
+                    getFragmentManager().beginTransaction().replace(android.R.id.content, new MyNotificationManagementFragment()).addToBackStack("MyNotificationManagementFragment").commit();
                     PreferenceManager.setDefaultValues(getActivity().getApplicationContext(), getResources().getString(R.string.shared_pref_key),
                             Context.MODE_PRIVATE, R.xml.preferences, false);
                     return false;
@@ -187,8 +152,9 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
             super.onCreate(savedInstanceState);
             PreferenceManager.setDefaultValues(getActivity(),
                     R.xml.notificationmanagement, true);
-            addPreferencesFromResource( R.xml.notificationmanagement);
+            addPreferencesFromResource(R.xml.notificationmanagement);
             initialise();
+            setRetainInstance(true);
         }
 
         @Override
@@ -202,7 +168,7 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
 
         }
 
-        public void initialise(){
+        public void initialise() {
             PreferenceScreen preferenceScreen = this.getPreferenceScreen();
 
             // create preferences manually
@@ -211,7 +177,7 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
             preferenceScreen.addPreference(preferenceCategory);
 
             // read shared preferences and use stored vales (if they exist)
-            for (int i=0; i<appInfoList.size(); i++){
+            for (int i = 0; i < appInfoList.size(); i++) {
                 final String appPkg = appInfoList.get(i).getAppPkg();
                 SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
                 boolean defaultValue = true;
@@ -220,7 +186,7 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
                     SwitchPreference preference = new SwitchPreference(preferenceScreen.getContext());
                     preference.setTitle(appInfoList.get(i).getAppName());
                     preference.setIcon(appInfoList.get(i).getAppIcon());
-                    if (!savedValue){
+                    if (!savedValue) {
                         preference.setChecked(false);
                     } else {
                         preference.setChecked(true);
@@ -243,7 +209,7 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
         }
     }
 
-    public void deleteNotifications(DataMap dataMap){
+    public void deleteNotifications(DataMap dataMap) {
         String WEARABLE_DATA_PATH = "/wearable_data";
 
         //Requires a new thread to avoid blocking the UI
@@ -273,6 +239,11 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
     // Connect to the data layer when the Activity starts
     @Override
     protected void onStart() {
@@ -285,10 +256,12 @@ public class MainActivity extends PreferenceActivity implements GoogleApiClient.
         super.onResume();
     }
 
-    // Send a data object when the data layer connection is successful.
+    /*
+     * Send a data object when the data layer connection is successful.
+     */
     @Override
     public void onConnected(Bundle connectionHint) {
-//        Log.i("MainActivity", "GoogleApiClient connected");
+        //        Log.i("MainActivity", "GoogleApiClient connected");
     }
 
     // Disconnect from the data layer when the Activity stops
